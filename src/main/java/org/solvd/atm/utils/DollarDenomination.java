388cba29597f841c6a57a0cda0bd486f09d75779
vlzJuan <valencellojuan@gmail.com>
2024-11-03 18:07:41 -0300

    - Added an ```int value``` parameter to store the numeric value of the DollarDenomination enum, and its getter. - The getter will be used to perform calculations for ach dollar value. - Added the constructor to initialize the ```int value``` attribute

--- src/main/java/org/solvd/atm/utils/DollarDenomination.java
+++ src/main/java/org/solvd/atm/utils/DollarDenomination.java
@@ -1,11 +1,22 @@
 package org.solvd.atm.utils;
 
 public enum DollarDenomination {
-    $1,
-    $2,
-    $5,
-    $10,
-    $20,
-    $50,
-    $100;
+    $1(1),
+    $2(2),
+    $5(5),
+    $10(10),
+    $20(20),
+    $50(50),
+    $100(100);
+
+    private final int value;
+
+    DollarDenomination(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
 }
