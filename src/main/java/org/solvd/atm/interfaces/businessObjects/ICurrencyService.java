6138303d2c6d538f6022fc7eea838bb43cf5cef1
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-11-03 18:22:46 -0700

    findCurrencyByName

--- src/main/java/org/solvd/atm/interfaces/businessObjects/ICurrencyService.java
+++ src/main/java/org/solvd/atm/interfaces/businessObjects/ICurrencyService.java
@@ -8,4 +8,5 @@ public interface ICurrencyService {
     List<CurrencyDTO> getAccountCurrenciesBalanceByAccountNum(String accountNumber);
     List<CurrencyDTO> getAccountCurrencies(String accountNumber);
     boolean validateAccountCurrencyBalance(String accountNumber,Double amount,String currency);
+    CurrencyDTO findCurrencyByName(String currencyName);
 }
