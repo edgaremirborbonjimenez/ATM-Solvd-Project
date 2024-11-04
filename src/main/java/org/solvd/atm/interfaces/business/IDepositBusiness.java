268ee24490503bc6f72f004c6c76d40d88f0d572
Favio <faugconti@gmail.com>
2024-11-04 00:58:47 -0300

    [refactor] changes in IDepositBusiness interface in business layer

--- src/main/java/org/solvd/atm/interfaces/business/IDepositBusiness.java
+++ src/main/java/org/solvd/atm/interfaces/business/IDepositBusiness.java
@@ -1,14 +1,17 @@
 package org.solvd.atm.interfaces.business;
 
+import org.solvd.atm.domain.ATM;
 import org.solvd.atm.dtos.AccountDTO;
 import org.solvd.atm.dtos.CurrencyDTO;
 import org.solvd.atm.dtos.DepositDTO;
+import org.solvd.atm.utils.DollarDenomination;
 
 import java.util.List;
 import java.util.Map;
 
 public interface IDepositBusiness {
     List<CurrencyDTO> getAccountCurrencies();
-    DepositDTO deposit(Map<Integer,Integer> denomination, Double amount, String currency);
+    DepositDTO deposit(Map<DollarDenomination,Integer> denomination, int amount, String currency);
     void setSessionAccountReference(AccountDTO accountReference);
+    void setATM(ATM ATM);
 }
