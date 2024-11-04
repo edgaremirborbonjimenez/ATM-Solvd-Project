fc9ded9cf4962417107a62897458c449450207d1
vlzJuan <valencellojuan@gmail.com>
2024-11-04 00:31:28 -0300

    -Declared a basic body for the Denominator class -Declared the method used to create an upper-bound map, to prevent over-calculations when breaking down an amount to withdraw in dollar bills.

--- src/main/java/org/solvd/atm/utils/Denominator.java
+++ src/main/java/org/solvd/atm/utils/Denominator.java
@@ -1,4 +1,24 @@
 package org.solvd.atm.utils;
 
-public abstract class Denominator {
+import java.util.EnumMap;
+
+public class Denominator {
+
+
+
+    /**
+     * Method that returns the upper bound limits for withdraw-breakdown
+     * @param AtmBillMap    , the map holding the amount of bills per bill-type within
+     *                      the current ATM in use.
+     * @return  An enumMap containing the upper limits used for calculations in withdraw-breakdown
+     */
+    public EnumMap<DollarDenomination, Integer> upperBounds(
+            EnumMap<DollarDenomination,Integer> AtmBillMap){
+        EnumMap<DollarDenomination, Integer> ret = new EnumMap<>(DollarDenomination.class);
+
+        return ret;
+    }
+
+
+
 }
