a14d3c21dd47c2b68455cd8d6577ed796f679cd4
Maximiliano Figueroa <mfigueroasemorile.laba@solvd.com>
2024-11-06 20:04:29 -0300

    Adding BalanceScreen logic

--- src/main/java/org/solvd/atm/interfaces/presentation/IBalanceScreen.java
+++ src/main/java/org/solvd/atm/interfaces/presentation/IBalanceScreen.java
@@ -1,5 +1,9 @@
 package org.solvd.atm.interfaces.presentation;
 
+import org.solvd.atm.implementations.business.BalanceBusiness;
+import org.solvd.atm.interfaces.business.IBalanceBusiness;
+
 public interface IBalanceScreen {
     void showAllCurrenciesBalance();
+    void setBalanceBusiness(IBalanceBusiness balanceBusiness);
 }
